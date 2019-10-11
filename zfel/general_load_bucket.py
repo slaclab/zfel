09ca86ed17e9f5cf02acad2e9653bd910a6ce362
Xiao <xiaozhg@stanford.edu>
2019-10-10 23:57:29 -0700

    Change the functions to be simplier

--- zfel/general_load_bucket.py
+++ zfel/general_load_bucket.py
@@ -39,9 +39,10 @@ def general_load_bucket(npart,gbar,delg,iopt,Ns,coopLength,resWavelength,particl
             location=int(s_all[k]/(dels*coopLength))
             N_input[location]+=1
             gam_step[location].append(gam_all[k])
+        N_real=N_input/np.max(N_input)*Ns
         #generate theta and gamma
-        thet_init=make_theta(npart,iopt,N_input,Ns,s_steps)
-        gam_init=make_gamma(gam_step,s_steps,npart,hist_steps)
+        thet_init=make_theta(npart,N_real)
+        gam_init=make_gamma(gam_step,npart,hist_steps)
 
     return thet_init,gam_init
 
@@ -94,59 +95,55 @@ def load_bucket(n,gbar,delg,iopt,Ns):
                 thet[i*M+j]=2*np.pi*(j+1)/M+2*np.random.rand(1)*effnoise
     return thet,gam
 
-def make_theta(n,iopt,N_input,Ns,s_steps):
+def make_theta(n,N_real):
     '''
     random initialization of the beam load_bucket
     inputs:
     n               # n-macro-particles per bucket
-    iopt            # 5=SASE, 4=seeded
-    N_input         # Input number of particles in different intervals along the beam
-    Ns              # N electrons per s-slice at maximum current
-    s_steps         # n-sample points along bunch length
+    N_real          # real number of particles in each bucket
     outputs:
     thet_init       # all buckets macro particles position
     '''
     
-    thet_init=np.zeros((s_steps,n))
-    if iopt==5:
-        M=32  # number of particles in each beamlet
-        nb= int(np.round(n/M) )    #number of beamlet via Fawley between 64 to 256 (x16=1024 to 4096)
-        if M*nb!=n:
-            raise ValueError('n must be a multiple of 4')
+    steps=N_real.shape[0]
+    thet_init=np.zeros((steps,n))
+    M=32  # number of particles in each beamlet
+    nb= int(np.round(n/M) )    #number of beamlet via Fawley between 64 to 256 (x16=1024 to 4096)
+    if M*nb!=n:
+        raise ValueError('n must be a multiple of 4')
         
-        for k in range(s_steps):
-            N_real=N_input[k]/np.max(N_input)*Ns
-            effnoise = np.sqrt(3*M/(N_real/nb))    # Penman algorithm for Ns/nb >> M
-            for i in range(nb):       
-                for j in range(M):
-                    thet_init[k,i*M+j]=2*np.pi*(j+1)/M+2*np.random.rand(1)*effnoise
+    for k in range(steps):
+        effnoise = np.sqrt(3*M/(N_real[k]/nb))    # Penman algorithm for Ns/nb >> M
+        for i in range(nb):       
+            for j in range(M):
+                thet_init[k,i*M+j]=2*np.pi*(j+1)/M+2*np.random.rand(1)*effnoise
     return thet_init
 
 
 
-def make_gamma(gam_step,s_steps,npart,hist_steps):
+def make_gamma(gam_step,npart,hist_steps):
     '''
     gam_step        # input particles' gamma values in different intervals along the beam
-    s_steps         # n-sample points along bunch length
     npart           # n-macro-particles per bucket
     hist_steps      # number of intervals to generate the histogram of gamma value in a bucket
     outputs:
     gam_init        # all buckets macro particles energy
     '''
+    steps=len(gam_step)
     if np.min(gam_step)==np.max(gam_step):
         #deal with the case that all input gamma values are the same
-        gam_init=np.ones((s_steps,npart))*np.max(gam_step)
+        gam_init=np.ones((steps,npart))*np.max(gam_step)
         print('Warning, all input gamma values are the same!')
     else:
         #make histogram for gamma
         lowbound=np.min(gam_step)
         upbound=np.max(gam_step)+1e-10
-        gam_hist=np.zeros((s_steps,hist_steps))
-        for j in range(s_steps):
+        gam_hist=np.zeros((steps,hist_steps))
+        for j in range(steps):
             gam_hist[j],bins=np.histogram(np.array(gam_step[j]),bins=np.linspace(lowbound, upbound, num=hist_steps+1))
         plt.figure()
-        _=plt.hist(np.array(gam_step[0]),bins=np.linspace(lowbound, upbound, num=hist_steps+1))
-        plt.title('Input gamma histogram')
+        #_=plt.hist(np.array(gam_step[0]),bins=np.linspace(lowbound, upbound, num=hist_steps+1))
+        #plt.title('Input gamma histogram')
         gam_hist/=np.sum(gam_hist,axis=1,keepdims=True)
 
         #make cdf
@@ -154,16 +151,16 @@ def make_gamma(gam_step,s_steps,npart,hist_steps):
         gam_cdf[:,0]=gam_hist[:,0]
         for j in range(1,hist_steps):
             gam_cdf[:,j]=gam_hist[:,j]+gam_cdf[:,j-1]
-        gam_cdf=np.concatenate((np.zeros((s_steps,1)),gam_cdf),axis=1)
+        gam_cdf=np.concatenate((np.zeros((steps,1)),gam_cdf),axis=1)
 
         #make gamma
-        x=np.random.rand(s_steps,npart)
-        gam_init=np.zeros((s_steps,npart))
-        for j in range(s_steps):
+        x=np.random.rand(steps,npart)
+        gam_init=np.zeros((steps,npart))
+        for j in range(steps):
             gam_init[j]=np.interp(x[j], gam_cdf[j], bins)
-        plt.figure()
-        _=plt.hist(gam_init[0],bins=np.linspace(lowbound, upbound, num=hist_steps+1))
-        plt.title('Sampled gamma histogram')
+        #plt.figure()
+        #_=plt.hist(gam_init[0],bins=np.linspace(lowbound, upbound, num=hist_steps+1))
+        #plt.title('Sampled gamma histogram')
     return gam_init
 
 
